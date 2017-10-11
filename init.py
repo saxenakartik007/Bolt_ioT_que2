aaad7139eee990cab42b9e111850aba98a99086d
Kartik <saxenakartik007>
2017-10-11 17:48:32 +0530

    Finished

--- init.py
+++ init.py
@@ -1,7 +1,11 @@
 import requests
 import pandas as pd
 from datetime import datetime
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
+plotly.tools.set_credentials_file(username='saxenakartik007', api_key='0OI5ZQTHcC6DXa885Gxs')
 
 req = requests.get(
     "http://beta.boltiot.com/fetchFromTable?fields=time_stamp,hum&duration=month&deviceName=BOLT1351489&from=&to=")
@@ -16,4 +20,16 @@ for datetimestring, humd in data["data"]:
     humd_data.append(humd)
 
 df = pd.DataFrame({"Humidity": pd.Series(humd_data, index=date_data)})
-print df
+
+
+data = [go.Scatter(x=df.index, y=df.Humidity)]
+
+layout_new = go.Layout(
+    yaxis=dict(
+        range=[20, 80]
+    )
+)
+
+fig = go.Figure(data=data, layout=layout_new)
+
+py.plot(fig, filename='axes-range-manual')
